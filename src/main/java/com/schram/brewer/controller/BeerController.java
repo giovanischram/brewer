b67642c463bc125c61ded8b5b049d878f83dbe99
Giovani Schram <schram.giovani@gmail.com>
2016-08-22 08:11:29 -0300

    Configurando thymeleaf.

--- src/main/java/com/schram/brewer/controller/BeerController.java
+++ src/main/java/com/schram/brewer/controller/BeerController.java
@@ -1,9 +1,13 @@
 package com.schram.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BeerController {
 
-	
+	@RequestMapping("/beers/new")
+	public String view() {
+		return "beer/BeerRegister";
+	}
 }
