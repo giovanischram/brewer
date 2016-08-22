0e44660c73af745b2115f372dc544c74728c8467
Giovani Schram <schram.giovani@gmail.com>
2016-08-22 09:09:25 -0300

    Iniciando cadastro de cerveja.

--- src/main/java/com/schram/brewer/controller/BeerController.java
+++ src/main/java/com/schram/brewer/controller/BeerController.java
@@ -2,12 +2,23 @@ package com.schram.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.schram.brewer.model.Beer;
 
 @Controller
 public class BeerController {
 
 	@RequestMapping("/beers/new")
 	public String view() {
+		return "beer/BeerRegister";
+	}
+
+	@RequestMapping(value = "/beers/new", method = RequestMethod.POST)
+	public String save(Beer beer) {
+		System.out.println(">>>>>>>> Saving new beer...");
+		System.out.println(">>>>>>>> SKU: " + beer.getSku());
+		System.out.println(">>>>>>>> Name: " + beer.getName());
 		return "beer/BeerRegister";
 	}
 }
